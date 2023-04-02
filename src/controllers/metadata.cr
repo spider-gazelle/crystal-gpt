76fecf98fd81145bea52c1db39ab0a5e2ef07a08
Stephen von Takach <steve@place.technology>
2023-04-02 22:08:16 +1000

    fix metadata

--- src/controllers/metadata.cr
+++ src/controllers/metadata.cr
@@ -2,24 +2,27 @@
 class Metadata < Application
   base "/.well-known"
 
+  NAME = App::NAME.gsub(/\-|\_/, ' ')
+
   # The manifest file that defines the plugin
   # it's using the macro DSL so it is not included
   # in the OpenAPI docs that are provided to ChatGPT
   get "/ai-plugin.json", :ai_plugin_json do
     host = "#{request_protocol}://#{request.headers["Host"]?}"
+    description = App::DESCRIPTION || raise "please provide a description in your shard.yml"
 
     render(json: {
       "schema_version":        "v1",
-      "name_for_human":        "TODO Plugin",
-      "name_for_model":        "todo",
-      "description_for_human": "Plugin for managing a TODO list, you can add, remove and view your TODOs.",
-      "description_for_model": "Plugin for managing a TODO list, you can add, remove and view your TODOs.",
+      "name_for_human":        NAME,
+      "name_for_model":        NAME,
+      "description_for_human": description,
+      "description_for_model": description,
       "auth":                  {
         "type": "none",
       },
       "api": {
         "type":                  "openapi",
-        "url":                   "#{host}/openapi.yaml",
+        "url":                   "#{host}/.well-known/openapi.yaml",
         "is_user_authenticated": false,
       },
       "logo_url":       "#{host}/logo.png",
@@ -30,7 +33,7 @@ class Metadata < Application
 
   # this file is built as part of the docker build
   OPENAPI = File.exists?("openapi.yml") ? File.read("openapi.yml") : ActionController::OpenAPI.generate_open_api_docs(
-    title: App::NAME,
+    title: NAME,
     version: App::VERSION,
     description: App::DESCRIPTION || "OpenAPI docs for ChatGPT plugin"
   ).to_yaml
